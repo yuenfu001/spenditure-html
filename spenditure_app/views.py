38bddbf5a24fda2544924a791e79f1df2b1ee308
liu yuenfu <liuyuenfu@gmail.com>
2022-03-05 21:06:03 +0100

    created views to render templates

--- spenditure_app/views.py
+++ spenditure_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mainpage(request):
+    context = {}
+    return render(request,'index.html' ,context)
+
+def homepage(request):
+    context = {}
+    return render(request,'home.html' ,context)
+
+def services(request):
+    context = {}
+    return render(request,'services.html', context)
+
+def about(request):
+    context = {}
+    return render(request,'about.html', context)
+
+# def home(request):
+#     context = {}
+#     return render(request, context)
+
+# def home(request):
+#     context = {}
+#     return render(request, context)
